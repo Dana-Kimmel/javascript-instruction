13ce7bdab8a67bfc6a639d9bf72e14927d0e3d1c
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-15 16:47:57 -0500

    withHelp(youtube)

--- chapter1.js
+++ chapter1.js
@@ -1,9 +1,13 @@
 // console.log("Agnes" || "user");
 // console.log(true == "Agnes");
 
-let mood = "light";
-console.log(typeof mood);
-console.log(mood);
-mood = 5;
-console.log(typeof mood);
-console.log(mood);
+// let mood = "light";
+// console.log(typeof mood);
+// console.log(mood);
+// mood = 5;
+// console.log(typeof mood);
+// console.log(mood);
+
+let myStr = "Hello";
+let i = 3;
+console.log(myStr[3]);
