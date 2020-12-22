f9be422ea73e44ec657392e5e460ec3688f0d869
Dana-Kimmel <dear.prudence34@gmail.com>
2020-12-21 20:37:33 -0500

    init commit

--- scrabblehand.js
+++ scrabblehand.js
@@ -1,10 +1,29 @@
-
-
-
+let tileHand = [
+  { tile: "N", score: 1, bonus: 3 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+];
 function maximumScore(tileHand) {
-	let total = 0;
-	for (let i = 0; i< tileHand.length; i++) {
-		total += tileHand[i].score ;
-	}
-	return total;
+  let total = 0;
+  for (let i = 0; i < tileHand.length; i++) {
+    total += tileHand[i].score;
+  }
+  return total;
 }
+
+function calcScore(tileHand) {
+  let total = 0;
+
+  for (let tile of tileHand) {
+      
+    total += tile{"score"} ;
+    if (tile.bonus != undefined) {
+        total += tile.bonus;
+    }
+  }
+  return total;
+}
+
+let total = calcScore(tileHand);
+console.log(total);
